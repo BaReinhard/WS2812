9addb26e7b3b48d8d4ee98c241ac629f37bf4fa1
BaReinhard <brett@reinhards.us>
2018-01-01 13:21:29 -0800

    Fixed Timing

--- WS2812.c
+++ WS2812.c
@@ -9,27 +9,32 @@ extern void delay_T1H();
 extern void delay_TLD();
 
 int main(){
-  for(int i=0 ; i<24 ; i++){
-  if(i>16)
-  data[i] = 1;
-  else
-  data[i] = 0;
-}
+  init_red(data,23);
+
+
 DDRB = 0b11111111;
 PORTB = 0b00000000;
 send_bytes(data,24);
 }
-
+void init_red(int data[], int data_len){
+  for(int i=0 ; i<24 ; i++){
+  if(i>=8 && i <=15)
+  data[i] = 1;
+  else
+  data[i] = 0;
+}
+}
 
 
 void send_bytes(int data[], int data_len){
-  for(int z = 0;z<32;z++){
+  for(int z = 0;z<59;z++){
     for(int i=0;i < data_len;i++){
       send_bit(data[i]);
     } 
   }
     PORTB = 0b00000000;
-    _delay_ms(6);
+    _delay_ms(0.6);
+    // _delay_ms(6);
     send_bytes(data,data_len);
 }
 
